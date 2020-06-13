89e91dd0293b80dd39f0b359c97a5f4ad933b155
William Trout <troutw5593@gmail.com>
2020-06-12 22:43:06 -0500

    changed all functions, still not working.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-require("console.table");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const db = require("./db");
 const util = require("util");
 const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employees",
+});
+
+connection.connect();
 
 function init() {
   const logoText = logo({
@@ -70,7 +79,7 @@ async function loadEmployeeTrackerQuestions() {
 
   switch (choice) {
     case "VIEW_EMPLOYEES":
-      return viewEmployees();
+      return viewAllEmployees();
     case "ADD_NEW_EMPLOYEE":
       return addNewEmployee;
     case "REMOVE_FORMER_EMPLOYEE":
@@ -91,169 +100,192 @@ async function loadEmployeeTrackerQuestions() {
 }
 
 // functions
+
 // view employees
-async function viewEmployees() {
-  const employees = db.findAllEmployees((cb) => {
-    console.table(cb);
+
+function viewAllEmployees() {
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
     loadEmployeeTrackerQuestions();
   });
 }
 
-// add new employee
+// view departments, not working
 
-async function addNewEmployee() {
-  const employees = await db.findAllEmployees();
-  const roles = await db.findAllRoles();
-  const employee = await prompt(
-    {
-      name: "first_name",
-      message: "What is the new employee's first name?",
-    },
-    {
-      name: "last_name",
-      message: "What is the new employee's last name?",
-    }
-  );
-  const roleChoices = roles.map(({ id, title }) => ({
-    name: title,
-    value: id,
-  }));
-  const { roleId } = await prompt([
-    {
-      type: "list",
-      name: "roleId",
-      message: "What is the new employee's role.",
-      choices: roleChoices,
-    },
-  ]);
-  employee.role_id = roleId;
-  const managerChoices = employee.map(({ id, first_name, last_name }) => ({
-    name: `${first_name} ${last_name}`,
-    value: id,
-  }));
-  managerChoices.unshift({
-    name: "None",
-    value: null,
-  });
-  const { managerId } = await prompt({
-    type: "list",
-    name: "managerId",
-    message: "Who is the new employee's manager?",
-    choices: managerChoices,
-  });
-  employee.manager_id = managerId;
-  await db.createEmployee(employee);
-  console.log(`${employee.first_name} ${employee.last_name} has been added.`);
-  loadEmployeeTrackerQuestions();
-}
-
-// remove ex employees
-async function removeFormerEmployee() {
-  const employees = await db.findAllEmployees();
-  const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
-    name: `${first_name} ${last_name}`,
-    value: id,
-  }));
-  const { employeeId } = await prompt({
-    type: "list",
-    name: "employeeId",
-    message: "Who would you like to remove?",
-    choices: employeeChoices,
-  });
-  await db.removeFormerEmployee(employeeId);
-  console.log(`${employee.first_name} ${employee.last_name} has been removed.`);
-  loadEmployeeTrackerQuestions();
-}
-
-// update existing employees
-// update employee role ?????
-async function updateEmployeeRole() {
-  const employees = await db.findAllEmployees();
-  const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
-    name: `${first_name} ${last_name}`,
-    value: id,
-  }));
-  const { employeeId } = await prompt([
-    {
-      type: "list",
-      name: "employeeId",
-      message: "Who's role do you need to changed?",
-      choices: employeeChoices,
-    },
-  ]);
-  const roles = await db.findAllRoles();
-  const roleChoices = roles.map(({ id, title }) => ({
-    name: title,
-    value: id,
-  }));
-  const { roleId } = await prompt({
-    type: "list",
-    name: "roleId",
-    message: "What is their new role in the organization?",
-    choices: roleChoices,
-  });
-  await db.updateEmployeeRole(employeeId, roleId);
-  console.log(
-    `Employee role for ${employee.first_name} ${employee.last_name} has been changed.`
-  );
-  loadEmployeeTrackerQuestions();
-}
-
-// view all roles
-async function viewEmployeeRoles() {
-  const employeeRoles = db.findAllRoles((cb) => {
-    console.table(cb);
+function viewDepartments() {
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.table(res);
     loadEmployeeTrackerQuestions();
   });
 }
 
-// add new roles
-async function addEmployeeRoles() {
-  const departments = await db.findAllByDepartments;
-  const departmentChoices = departments.map(({ id, name }) => ({
-    name: name,
-    value: id,
-  }));
-  const role = await prompt([
-    {
-      name: "title",
-      message: "What is the name of the role being added?",
-    },
-    {
-      name: "salary",
-      message: "What is the salary for this role?",
-    },
-    {
-      type: "list",
-      name: "department_id",
-      message: "Which department does the role align with?",
-      choices: departmentChoices,
-    },
-  ]);
-  await db.addEmployeeRoles(role);
-  console.log(`${role.title} has been added to the database.`);
-  loadEmployeeTrackerQuestions();
+// view roles, not working
+
+function viewEmployeeRoles() {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    loadEmployeeTrackerQuestions();
+  });
 }
 
-//view departments
-async function viewDepartments() {
-  const departments = await db.findAllDepartments();
-  console.log("\n");
-  console.table(departments);
-  loadMainPrompts();
+// add department, not working
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "department",
+        type: "input",
+        message: "Add a New Department: ",
+      },
+    ])
+    .then((res) => {
+      connection.query(
+        `INSERT INTO department(department_name) VALUES ("${res.department}")`,
+        (err, res) => {
+          if (err) throw err;
+          loadEmployeeTrackerQuestions();
+          console.log("Department has been added!");
+        }
+      );
+    });
 }
 
-// add department
-async function addDepartment() {
-  const department = await prompt([
-    {
-      name: "name",
-      message: "What is the name of the department?",
-    },
-  ]);
-  await db.addDepartment(department);
-  console.log(`Added ${department.name} to the database`);
-  loadMainPrompts();
+// add roles, not working
+
+function addEmployeeRoles() {
+  connection.query(`SELECT * FROM department`, (err, department) => {
+    if (err) throw err;
+    const departmentList = department.map((d) => {
+      return {
+        name: d.department_name,
+        value: d.id,
+      };
+    });
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is the title of this new role?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary of this role?",
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "What department would you like to add this to?",
+          choices: departmentList,
+        },
+      ])
+      .then((res) => {
+        connection.query(
+          `INSERT INTO role(title, salary, department_id) VALUES ("${res.title}","${res.salary}","${res.department}")`,
+          (err, res) => {
+            if (err) throw err;
+            loadEmployeeTrackerQuestions();
+          }
+        );
+        console.log("Role has been added!");
+      });
+  });
+}
+
+// add new employee, not working
+
+function addNewEmployee() {
+  connection.query(`SELECT * FROM role`, (err, role) => {
+    if (err) throw err;
+    const roleList = role.map((r) => {
+      return {
+        name: r.title,
+        value: r.id,
+      };
+    });
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the first name of the new employee?",
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What the last name of the new employee?",
+        },
+        {
+          type: "list",
+          name: "role_id",
+          message: "What is the role of this new employee?",
+          choices: roleList,
+        },
+      ])
+      .then((res) => {
+        connection.query(
+          `INSERT INTO employee(first_name, last_name, role_id) 
+          VALUES ("${res.first_name}", "${res.last_name}", ${res.role_id})`,
+          (err, res) => {
+            if (err) throw err;
+            loadEmployeeTrackerQuestions();
+          }
+        );
+        console.log("Employee has been added!");
+      });
+  });
+}
+
+// update employee info, role... not working...
+
+function updateEmployeeRole() {
+  connection.query(`SELECT * FROM employee`, (err, employee) => {
+    if (err) throw err;
+    const allEmployees = employee.map((e) => {
+      return {
+        name: `${e.first_name} ${e.last_name}`,
+        value: e.id,
+      };
+    });
+    connection.query(`SELECT title, id FROM role`, (err, role) => {
+      if (err) throw err;
+      const updateRole = role.map((r) => {
+        return {
+          name: r.title,
+          value: r.id,
+        };
+      });
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "employee",
+            message: "Which Employee Role would you like to update?",
+            choices: allEmployees,
+          },
+          {
+            name: "role",
+            type: "list",
+            message: "What role are you adding?",
+            choices: updateRole,
+          },
+        ])
+        .then((res) => {
+          connection.query(
+            `UPDATE employee SET role_id=${res.role} WHERE id=${res.employee}`,
+            (err, res) => {
+              if (err) throw err;
+              loadEmployeeTrackerQuestions();
+            }
+          );
+        });
+    });
+  });
 }
 
 function quit() {
